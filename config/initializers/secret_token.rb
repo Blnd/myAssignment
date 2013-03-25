446734ae0b37073888306753b1f7731200883970
blnd <blnd.najmadin@auis.edu.iq>
2013-03-25 10:07:31 -0700

    first and last version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyAssignmet::Application.config.secret_token = '89eeceb823fe01a2fa39a8b8d8ee883713cb4776796a9904068d8931c0a4588412c22233f765ff8229d449c3dcb6adb9b84771fcb041511aa9ea00c6417fa2aa'
